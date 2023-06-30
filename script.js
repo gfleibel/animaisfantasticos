e9ead1aaee4346a66d845144f78d83d48a0e4f71
gabriel leibel <gfleibel@hotmail.com>
2023-06-30 15:13:43 -0300

    js exercises

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ const menu = document.getElementsByClassName('menu');
 const primeiraLi = document.querySelector('li');
 const primeiraUl = document.querySelector('ul');
 const linksInternos = document.querySelectorAll('.animais img');
+
 const linkInterno = document.querySelector('[href^="#"]');
+const images = document.querySelectorAll('img[src^="img/imagem"]');
+const h2Animais = document.querySelector('.animais-descricao h2');
+
+const paragrafos = document.querySelectorAll('p');
+const ultimoParagrafo = paragrafos[--paragrafos.length];
