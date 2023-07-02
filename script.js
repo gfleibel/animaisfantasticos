b730d41408b370bf2f30f81293cd039c29a5d9be
Gabriel Leibel <gfleibel@hotmail.com>
2023-07-02 11:39:26 -0300

    cleared script

--- script.js
+++
@@ -1,9 +0,0 @@
-const animaisSection = document.getElementById('animais');
-const gridSection = document.getElementsByClassName('grid-section');
-const contatoSection = document.getElementsByClassName('grid-section contato');
-const ul = document.getElementsByTagName('ul');
-const menu = document.getElementsByClassName('menu');
-const primeiraLi = document.querySelector('li');
-const primeiraUl = document.querySelector('ul');
-const linksInternos = document.querySelectorAll('.animais img');
-const linkInterno = document.querySelector('[href^="#"]');
