a22e71ee9afe2b365ac979db021256e9e4a3ca73
Gabriel Leibel <gfleibel@hotmail.com>
2023-07-02 13:49:25 -0300

    soft scroll

--- script.js
+++ script.js
@@ -44,3 +44,28 @@ function initAccordion() {
 };
 
 initAccordion();
+
+// soft scroll internal links
+
+function initSoftScroll() {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  if(internalLinks.length){
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+
+    internalLinks.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+};
+
+initSoftScroll();
