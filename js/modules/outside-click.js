1a68418f708409b5a505478792b5955f9952dac2
Gabriel Leibel <gfleibel@hotmail.com>
2023-07-11 11:26:26 -0300

    menu mobile

--- js/modules/outside-click.js
+++ js/modules/outside-click.js
@@ -3,9 +3,9 @@ export default function outsideClick(element, events, callback) {
   const outside = 'data-outside';
   if(!element.hasAttribute(outside)){
     events.forEach((userEvent) => {
-    html.addEventListener('click', handleOutsideClick);
-    element.setAttribute(outside, '');
+    setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
     });
+    element.setAttribute(outside, '');
   };
   function handleOutsideClick(event) {
     if(!element.contains(event.target)) {
--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initScrollAnimation from "./modules/scroll-animation.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initTabNav();
 initSoftScroll();
@@ -14,3 +15,4 @@ initScrollAnimation();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
